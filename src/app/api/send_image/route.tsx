fb96902d39bdefe9a5354e36d520488b78f7ba85
Gene Ni <geneisjuan@gmail.com>
2023-09-17 01:45:56 -0400

    fixed route and remove logo

--- src/app/api/send_image/route.tsx
+++ src/app/api/send_image/route.tsx
@@ -3,7 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
-  // console.log(body);
+  const response = await fetch("http:/10.191.94.216:2000/fapi/process_image", {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
 
   return NextResponse.json({
     status: 200,
--- src/app/camera/components/ImageHolder.tsx
+++ src/app/camera/components/ImageHolder.tsx
@@ -13,22 +13,24 @@ const ImageHolder = ({ src, image }: { src: string; image: string }) => {
   return (
     <>
       <div className=" flex flex-col justify-end align-middle h-full w-full ">
-        <div className="py-5 text-2xl">
-          <p className=" font-bold tracking-[.2rem]">Sophér</p>
-        </div>
         {src !== "" ? (
-          <div className=" flex flex-col">
-            <Image
-              src={src}
-              alt="Image Placeholder"
-              width={100}
-              height={100}
-              className=" w-full"
-            />
-            <div className=" flex flex-col justify-center h-10">
-              <p className=" text-gray-500">1/1</p>
+          <>
+            <div className=" flex flex-col">
+              <div className="py-5 text-2xl">
+                <p className=" font-bold tracking-[.2rem]">Sophér</p>
+              </div>
+              <Image
+                src={src}
+                alt="Image Placeholder"
+                width={100}
+                height={100}
+                className=" w-full"
+              />
+              <div className=" flex flex-col justify-center h-10">
+                <p className=" text-gray-500">1/1</p>
+              </div>
             </div>
-          </div>
+          </>
         ) : (
           <div className=" h-full flex flex-col align-middle justify-center">
             <h2 className=" text-slate-700">
